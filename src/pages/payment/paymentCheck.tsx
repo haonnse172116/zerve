5e11e2af32ae03cbd4e00e8cc7bbcab069cbc9bb
Hao Nguyen <haonnse172116@fpt.edu.vn>
2025-04-12 18:04:12 +0700

    aaaa

--- src/pages/payment/paymentCheck.tsx
+++ src/pages/payment/paymentCheck.tsx
@@ -11,18 +11,10 @@ const PaymentSuccess = () => {
   return (
     <div className="max-w-4xl mx-auto p-6 bg-gradient-to-r from-indigo-600 to-purple-600 rounded-xl shadow-xl text-white">
       <div className="text-center mb-8">
-        <h2 className="text-4xl font-extrabold">Thanh toán thành công!</h2>
+        <h2 className="text-4xl font-extrabold">Hoàn tất thủ tục đặt xe!</h2>
         <p className="mt-4 text-lg">Chúc mừng! Đặt xe của bạn đã hoàn tất thành công.</p>
       </div>
-
-      <div className="flex justify-center mt-6">
-        <img
-          src="https://img.vietqr.io/image/STB-070125660841-qr_only.png"
-          alt="QR Code"
-          className="w-64 h-64 mb-6 rounded-lg shadow-lg"
-        />
-      </div>
-
+      
       <div className="text-center mb-6">
         <p className="text-lg font-medium">
           Vui lòng kiểm tra chuyến đi của bạn để theo dõi quá trình thuê xe.
